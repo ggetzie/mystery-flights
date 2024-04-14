c663f0f7ca65a5a2c594cdf49c33ebd7f8f42c2a
Gabriel Getzie <gabe@kotsf.com>
2024-04-14 09:40:43 +0800

    comments, cleanup

--- script.py
+++ script.py
@@ -11,41 +11,45 @@ routes_file_json = pathlib.Path("data/flight_routes.json")
 
 
 def get_airport_codes():
+    # organize the airports data into a dictionary indexed by IATA code
+    # ignore airports without an IATA code
     if airports_file_json.exists():
         with airports_file_json.open(encoding="utf-8") as f:
             return json.load(f)
-    else:
-        with airports_file.open(encoding="utf-8") as f:
-            reader = csv.DictReader(f)
-            res = {
-                row["iata_code"]: {
-                    "name": row["name"],
-                    "latitude_deg": row["latitude_deg"],
-                    "longitude_deg": row["longitude_deg"],
-                }
-                for row in reader
-                if row["iata_code"]
+
+    with airports_file.open(encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        res = {
+            row["iata_code"]: {
+                "name": row["name"],
+                "latitude_deg": row["latitude_deg"],
+                "longitude_deg": row["longitude_deg"],
             }
-            airports_file_json.write_text(json.dumps(res, indent=2), encoding="utf-8")
-            return res
+            for row in reader
+            if row["iata_code"]
+        }
+        airports_file_json.write_text(json.dumps(res, indent=2), encoding="utf-8")
+        return res
 
 
 def get_routes():
+    # organize the flight route data into a dictionary of
+    # source airport -> list of possible destination airports
     airports = get_airport_codes()
     if routes_file_json.exists():
         with routes_file_json.open(encoding="utf-8") as f:
             return json.load(f)
-    else:
-        with routes_file.open(encoding="utf-8") as f:
-            reader = csv.DictReader(f)
-            res = defaultdict(list)
-            for row in reader:
-                if (
-                    row["stops"] == "0"
-                    and row["source_airport"] in airports
-                    and row["destination_airport"] in airports
-                ):
-                    # only nonstop flights to and from airports we have info for
-                    res[row["source_airport"]].append(row["destination_airport"])
-            routes_file_json.write_text(json.dumps(res, indent=2), encoding="utf-8")
-            return res
+
+    with routes_file.open(encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        res = defaultdict(list)
+        for row in reader:
+            # only nonstop flights to and from airports we have info for
+            if (
+                row["stops"] == "0"
+                and row["source_airport"] in airports
+                and row["destination_airport"] in airports
+            ):
+                res[row["source_airport"]].append(row["destination_airport"])
+        routes_file_json.write_text(json.dumps(res, indent=2), encoding="utf-8")
+        return res
